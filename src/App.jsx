54e7161dcc5010c5177d0126bab5a76b3e2fa360
JCJC87 <vende_2@hotmail.com>
2025-11-28 13:07:14 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Marquesina from './components/Marquesina.jsx';
+
+function App() {
+  return (
+    <div className="marquesina-container">
+      <Marquesina />
+    </div>
+  );
+}
+
+export default App;
